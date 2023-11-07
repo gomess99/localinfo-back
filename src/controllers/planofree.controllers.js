ce8dfd2c3d4190cd8bd4bfea8848d05bf7ccad34
gomess99 <gomess99@outlook.com>
2023-11-07 20:23:18 -0300

    Protegendo uma rota com autenticação JWT

--- src/controllers/planofree.controllers.js
+++ src/controllers/planofree.controllers.js
@@ -3,6 +3,24 @@ import {createService, findAllService} from "../services/planofree.service.js"
 const create = async (req, res) =>{
 
     try {
+        const { authorization } = req.headers;
+        
+        if(!authorization){
+            return res.send(401);
+        }
+
+        const parts = authorization.split(" ");
+
+        const [schema, token] = parts;
+
+        if (parts.length !==2) {
+            return res.send(401);
+        }
+
+        if(schema !== "Bearer"){
+            return res.send(401);
+        }
+
         const {carrossel, funcionamento} = req.body;
 
         if(!carrossel || !funcionamento){
