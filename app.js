d65345f67ce6db8b78f8662a2cb2ec6e057369b5
UarliBS <warllen2014@gmail.com>
2023-11-17 20:49:40 -0300

    Permitindo acesso ao CORS

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 //aqui configura a aplicação
+import cors from 'cors';
 import express from "express";
 import connectDatabase  from "./src/database/db.js";
 import "dotenv/config";
 import router from "./src/routes/index.js";
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    app.use(cors());
+    next();
+});
 
 connectDatabase();
 
