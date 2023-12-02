4cf1fb6ac2f55e93eb68fde735cfae37e781d377
UarliBS <warllen2014@gmail.com>
2023-12-02 20:03:59 -0300

    atuallização configuração do cors

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//aqui configura a aplicação
 import cors from 'cors';
 import express from "express";
 import connectDatabase  from "./src/database/db.js";
@@ -7,11 +6,12 @@ import router from "./src/routes/index.js";
 
 const app = express();
 
+// Configuração do CORS
+app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
-    app.use(cors());
     next();
 });
 
@@ -19,7 +19,5 @@ connectDatabase();
 
 app.use(express.json());
 app.use(router);
-//app.use("/user", userRoute);
-
 
 export default app;
