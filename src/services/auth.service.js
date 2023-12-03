92e64c4410f070032d3b2e876acfc1b37c3c3f62
gomess99 <gomess99@outlook.com>
2023-12-02 23:31:00 -0300

    Melhoria do gerador de token do auth pj

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,9 +3,9 @@ import bcrypt from "bcrypt";
 import "dotenv/config";
 import pessoajuridicarepositories from "../repositories/pessoajuridicarepositories.js";
 
-
-const generateToken = (id) =>
-  jwt.sign({ id: id }, process.env.SECRET_JWT, { expiresIn: 86400 });
+function generateToken(id) {
+  return jwt.sign({ id: id }, process.env.SECRET, { expiresIn: 86400 });
+}
 //tempo para expirar de 24h
 
 const loginServicePessoaJuridica = async ({ email, password }) => {
@@ -27,22 +27,22 @@ const loginServicePessoaJuridica = async ({ email, password }) => {
 };
 
 const loginServicePessoaFisica = async ({ email, password }) => {
-  const pessoajuridica =
+  const pessoafisica =
     await pessoajuridicarepositories.findByEmailPessoaJuridicaRepository(email);
 
-  if (!pessoajuridica) throw new Error("Wrong password or username");
+  if (!pessoafisica) throw new Error("Wrong password or username");
 
-  const isPasswordValid = await bcrypt.compare(
-    password,
-    pessoajuridica.password
-  );
+  const isPasswordValid = await bcrypt.compare(password, pessoafisica.password);
 
   if (!isPasswordValid) throw new Error("Invalid password");
 
-  const token = generateToken(pessoajuridica.id);
+  const token = generateToken(pessoafisica.id);
 
   return token;
 };
 
-
-export default{ loginServicePessoaJuridica, loginServicePessoaFisica, generateToken };
+export default {
+  loginServicePessoaJuridica,
+  loginServicePessoaFisica,
+  generateToken,
+};
